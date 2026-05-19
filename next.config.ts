aa0d7d6d8f39f90490c87289c68474db1eb24a7c
yaniii <ferhatenyani19@gmail.com>
2026-05-19 22:43:57 +0200

    gitignore

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["192.168.1.39"],
+  allowedDevOrigins: ["10.80.52.4"],
 };
 
 export default nextConfig;
